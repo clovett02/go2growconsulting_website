6c13cc191e4e7dd39c358fb601d43be836d0bd51
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-13 14:54:23 -0400

    added routes for 'contact' and 'about'

--- website/views.py
+++ website/views.py
@@ -5,3 +5,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def Home():
     return render_template('Home.html', pagename="Go 2 Grow")
+
+@views.route('/contact')
+def Contact():
+    return render_template('Contact.html', pagename="Contact Go 2 Grow")
+
+@views.route('/about')
+def About():
+    return render_template('About.html', pagename="About Go 2 Grow")
